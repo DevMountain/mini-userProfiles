7687ebe04a640db4385964e103f4d827471153ea
Thai <Thai.Tran5656@gmail.com>
2016-09-27 21:26:21 -0600

    css needed

--- controller.js
+++ controller.js
@@ -1,5 +1,11 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
-    $scope.thisAppIsBroken = "This angular app is working";
-   
+angular.module('userProfiles')
+.controller('MainController', function($scope, mainService){
+
+    $scope.getUsers = function(){
+         $scope.users = mainService.getUsers();
+      }
+
+      $scope.getUsers();
+
+        $scope.toggleFavorite = mainService.toggleFavorite;
 })
